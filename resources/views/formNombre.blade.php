3c6809b6541c1b22f9b3a83776058abfbfe3b52b
zaballuko <zaballuko@gmail.com>
2018-10-25 11:17:06 +0200

    bootstrap formNombre

--- resources/views/formNombre.blade.php
+++ resources/views/formNombre.blade.php
@@ -1,17 +1,17 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title>formNombre</title>
-</head>
-<body>
+@extends('layouts.layout-calculadora')
+@section('title','formPedirNombre')
+
+<!-- donde quires que vaya y lo que quieres -->
+
+	@section('content')
+	<div class="content">
 	<h1>Reto Calculadora</h1>
 	<form action="/pasarNombre" method="post">
 			<input type="hidden" name="_token" value="{{ csrf_token() }}"><br>
-			Introduce tu nombre:<br>
+			<label for="exampleInputEmail1">Introduce tu nombre:</label><br>
 			<input type="text" name="nombre"><br>
-			<p>El nombre introducido aparecera en la calculadora</p><br>
-			<button type="submit">Enviar</button>
+			<p>El nombre introducido aparecera en la calculadora</p>
+			<button type="submit"  class="btn btn-success">Enviar</button>
 		</form>
-</body>
-</html>
+	</div>
+@endsection
